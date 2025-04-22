6d2ac78765a3ea8061ee4faff36fbf37838e90dd
Douglas Carmichael <dcarmich@dcarmichael.net>
2025-04-22 01:59:12 -0400

    Removed tabbing on main window and added initial OLED/7SEG switching on View menu.

--- DelugeDisplay/DelugeDisplayApp.swift
+++ DelugeDisplay/DelugeDisplayApp.swift
@@ -7,8 +7,19 @@
 
 import SwiftUI
 
+enum DisplayMode {
+    case oled
+    case sevenSeg
+}
+
 @main
 struct DelugeDisplayApp: App {
+    @State private var displayMode: DisplayMode = .oled
+
+    init()
+    {
+        NSWindow.allowsAutomaticWindowTabbing = false
+    }
     var body: some Scene {
         WindowGroup {
             ContentView()
@@ -57,6 +68,22 @@ struct DelugeDisplayApp: App {
                     NSApplication.shared.terminate(nil)
                 }
                 .keyboardShortcut("q", modifiers: .command)
+            }
+            
+            // Custom View menu only
+            CommandGroup(replacing: .sidebar) {
+                Divider()
+                Toggle("Show OLED", isOn: Binding(
+                    get: { displayMode == .oled },
+                    set: { if $0 { displayMode = .oled } }
+                ))
+                    .keyboardShortcut("1", modifiers: .command)
+                
+                Toggle("Show 7SEG", isOn: Binding(
+                    get: { displayMode == .sevenSeg },
+                    set: { if $0 { displayMode = .sevenSeg } }
+                ))
+                    .keyboardShortcut("2", modifiers: .command)
             }
         }
     }
