44120f066ac43a732a541534db6ccfb7aeacfb6e
Douglas Carmichael <dcarmich@dcarmichael.net>
2025-04-21 23:35:44 -0400

    Initial Commit

---
+++ DelugeDisplay/DelugeDisplayApp.swift
@@ -0,0 +1,17 @@
+//
+//  DelugeDisplayApp.swift
+//  DelugeDisplay
+//
+//  Created by Douglas Carmichael on 4/21/25.
+//
+
+import SwiftUI
+
+@main
+struct DelugeDisplayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
